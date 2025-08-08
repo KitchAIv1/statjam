98fd624170693e9e66e2d982eab4e5ed83e0026e
chieftitan88 <chief.store88@gmail.com>
2025-08-08 14:33:14 -0400

    Remove Landing V2 and Figma export; reverting to V1 only per direction

--- src/app/landing-v2/page.tsx
+++
@@ -1,8 +0,0 @@
-// no 'use client' here: this page composes client components from Figma as children
-
-import { LandingPage } from '../../../LANDING PAGE FIGMA/StatJam Landing Page (1)/components/LandingPage';
-
-export default function LandingV2Page() {
-  return <LandingPage />;
-}
-
